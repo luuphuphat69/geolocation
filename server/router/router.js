1c38048db1f040dad2f4b8469f850eeb12b22807
luuphuphat69 <luuphuphat69@gmail.com>
2025-01-07 15:13:28 +0700

    push-up notification

--- server/router/router.js
+++ server/router/router.js
@@ -1,6 +1,7 @@
 const locationController = require('../controller/location');
 const lambdaController = require('../controller/lambdaTrigger');
 const weatherController = require('../controller/weather');
+const FCMController = require('../controller/FCMController');
 
 const router = require('express').Router();
 router.get('/location', locationController.retrieveData);
@@ -11,4 +12,6 @@ router.get('/lambda/unsub', lambdaController.unsubcribe);
 router.get('/weather/current', weatherController.GetCurrentWeatherData);
 router.get('/weather/forecast', weatherController.GetWeatherForecastData);
 router.get('/weather/airpollution', weatherController.GetAirPollutionData);
+
+router.post('/FCM/token', FCMController.saveToken);
 module.exports = router;
