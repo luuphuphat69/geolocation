70a48e15dc8ab56697ca0adb8977a11de79634eb
luuphuphat69 <luuphuphat69@gmail.com>
2025-07-23 23:44:47 +0700

    hourly forecast update

--- geolocation/src/components/comps/hourlyforecast.jsx
+++ geolocation/src/components/comps/hourlyforecast.jsx
@@ -1,15 +1,32 @@
 import '../../css/hourlyforecast.css'
-import { useState, useEffect } from 'react';
 
-const HourlyForecast = (hourlyForecastData) => {
+const HourlyForecast = ({ hourlyForecastData }) => {
+    if (
+        !hourlyForecastData ||
+        !hourlyForecastData.forecast ||
+        !hourlyForecastData.forecast.forecastday ||
+        !hourlyForecastData.forecast.forecastday[0] ||
+        !hourlyForecastData.forecast.forecastday[0].hour
+    ) {
+        return <div>Loading hourly forecast...</div>;
+    }
 
     return (
-        <div class="full-day-forecast">
-            <h4 class="forecast-title">24-Hour Forecast</h4>
-            <div class="forecast-scroll" id="forecast-scroll">
-
+        <div className="full-day-forecast">
+            <h4 className="forecast-title">24-Hour Forecast</h4>
+            <div className="forecast-scroll" id="forecast-scroll">
+                {hourlyForecastData.forecast.forecastday[0].hour.map((item, index) => (
+                    <div key={index} className={`forecast-item ${item.isCurrent ? 'forecast-item--current' : ''}`}>
+                        <p className="forecast-time">{item.time.split(' ')[1]}</p>
+                        <div className="forecast-icon">
+                            <img src={`https:${item.condition.icon}`} alt="weather icon" />
+                        </div>
+                        <p className="forecast-temp">{item.temp_c}°</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
-}
+};
+
 export default HourlyForecast;
