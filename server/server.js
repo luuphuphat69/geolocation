db20094059cd9a5eff815ef9449e74a62b3510d1
luuphuphat69 <luuphuphat69@gmail.com>
2025-09-08 19:20:55 +0700

    fix cors

--- server/server.js
+++ server/server.js
@@ -1,19 +1,13 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const morgan = require('morgan');
 const router = require('./router/router');
 
 const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const CLIENT_BASE = "https://www.geolocation.space"
-//const CLIENT_BASE = "http://localhost:5173"
+const CLIENT_BASE = "https://www.geolocation.space";
 
 const corsOptions = {
-  origin: [CLIENT_BASE],
+  origin: CLIENT_BASE,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: [
@@ -27,10 +21,14 @@ const corsOptions = {
     'Authorization'
   ],
 };
-app.use(cors(corsOptions));
-app.use(morgan("common"));
 
-// Routes
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // handle preflight
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('common'));
+
 app.use('/v1', router);
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
