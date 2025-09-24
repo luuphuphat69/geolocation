376e79c95285362d6a688c42c762991a37c1050b
luuphuphat69 <luuphuphat69@gmail.com>
2025-09-24 20:53:26 +0700

    update server reconnect for db

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const PORT = 3000;
 
 (async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION);
-    console.log('Connected to MongoDB');
-
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB disconnected! Reconnecting...");
+      connectDB();
+    });
+    connectDB();
+    
     app.listen(PORT, () => {
       console.log(`App listening on ${PORT}`);
     });
@@ -15,3 +18,12 @@ const PORT = 3000;
     console.error('Error connecting to MongoDB:', error);
   }
 })();
+
+function connectDB() {
+  mongoose.connect(process.env.MONGODB_CONNECTION, { autoReconnect: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => {
+      console.error("MongoDB connection error:", err);
+      setTimeout(connectDB, 5000); // retry after 5s
+    });
+}
